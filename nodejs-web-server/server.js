a38bba02e35dff8ecaef65d80668b4d95cfdf842
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-05-28 23:20:39 +0700

    Handle the response status

--- nodejs-web-server/server.js
+++ nodejs-web-server/server.js
@@ -8,12 +8,15 @@ const requestListener = (request, response) => {
 
     if (url === '/') {
         if (method === 'GET') {
+            response.statusCode = 200;
             response.end('<h1>Ini adalah homepage</h1>');
         } else {
-            response.end('<h1>Halaman tidak dapat diakses dengan ${method} request</h1>');
+            response.statusCode = 400;
+            response.end(`<h1>Halaman tidak dapat diakses dengan ${method} request</h1>`);
         }
     } else if (url === '/about') {
         if (method === 'GET') {
+            response.statusCode = 200;
             response.end('<h1>Halo!, Ini adalah halaman about</h1>')
         } else if (method === 'POST') {
             let body = [];
@@ -25,12 +28,15 @@ const requestListener = (request, response) => {
             request.on('end', () => {
                 body = Buffer.concat(body).toString()
                 const {name} = JSON.parse(body);
+                response.statusCode = 200;
                 response.end(`<h1>Hai, ${name}! Ini adalah halaman about</h1>`);
             });
         } else {
-            response.end('<h1>Halaman tidak dapat diakses dengan ${method} request</h1>');
+            response.statusCode = 400;
+            response.end(`<h1>Halaman tidak dapat diakses dengan ${method} request</h1>`);
         }
     } else {
+        response.statusCode = 404;
         response.end('<h1>Halaman tidak ditemukan!</h1>');
     }
 }
