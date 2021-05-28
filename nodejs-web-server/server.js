27cd3dcbe441d8165441e0ccce6fefc867b5a5aa
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-05-28 23:49:29 +0700

    Change the request body to JSON

--- nodejs-web-server/server.js
+++ nodejs-web-server/server.js
@@ -11,19 +11,25 @@ const requestListener = (request, response) => {
             response.statusCode = 200;
             response.setHeader('Content-Type', 'application/json');
             response.setHeader('X-Powered-By', 'NodeJS');
-            response.end('<h1>Ini adalah homepage</h1>');
+            response.end(JSON.stringify({
+                message: 'Ini adalah homepage'
+            }));
         } else {
             response.statusCode = 400;
             response.setHeader('Content-Type', 'application/json');
             response.setHeader('X-Powered-By', 'NodeJS');
-            response.end(`<h1>Halaman tidak dapat diakses dengan ${method} request</h1>`);
+            response.end(JSON.stringify({
+                message: `Halaman tidak dapat diakses dengan ${method} request`
+            }));
         }
     } else if (url === '/about') {
         if (method === 'GET') {
             response.statusCode = 200;
             response.setHeader('Content-Type', 'application/json');
             response.setHeader('X-Powered-By', 'NodeJS');
-            response.end('<h1>Halo!, Ini adalah halaman about</h1>')
+            response.end(JSON.stringify({
+                message: 'Halo!, Ini adalah halaman about'
+            }));
         } else if (method === 'POST') {
             let body = [];
 
@@ -37,19 +43,25 @@ const requestListener = (request, response) => {
                 response.statusCode = 200;
                 response.setHeader('Content-Type', 'application/json');
                 response.setHeader('X-Powered-By', 'NodeJS');
-                response.end(`<h1>Hai, ${name}! Ini adalah halaman about</h1>`);
+                response.end(JSON.stringify({
+                    message: `Hai, ${name}! Ini adalah halaman about`
+                }));
             });
         } else {
             response.statusCode = 400;
             response.setHeader('Content-Type', 'application/json');
             response.setHeader('X-Powered-By', 'NodeJS');
-            response.end(`<h1>Halaman tidak dapat diakses dengan ${method} request</h1>`);
+            response.end(JSON.stringify({
+                message: `Halaman tidak dapat diakses dengan ${method} request`
+            }));
         }
     } else {
         response.statusCode = 404;
         response.setHeader('Content-Type', 'application/json');
         response.setHeader('X-Powered-By', 'NodeJS');
-        response.end('<h1>Halaman tidak ditemukan!</h1>');
+        response.end(JSON.stringify({
+            message: 'Halaman tidak ditemukan!'
+        }));
     }
 }
 
