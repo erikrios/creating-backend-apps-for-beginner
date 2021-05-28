517f166d36977dcb26354f6f424bf730cd2c67f0
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-05-28 12:35:10 +0700

    Add an example code of NodeJs file system

---
+++ nodejs-basic/filesystem/index.js
@@ -0,0 +1,12 @@
+const path = require('path');
+const fs = require('fs');
+
+const fileReadCallback = (error, data) => {
+    if (error) {
+        console.log('Gagal membaca berkas');
+        return;
+    }
+    console.log(data);
+}
+
+fs.readFile(path.resolve(__dirname, 'notes.txt'), 'UTF-8', fileReadCallback);
