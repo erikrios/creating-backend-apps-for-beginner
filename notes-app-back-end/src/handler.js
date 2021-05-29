9cdd6c146d0248bd7cc2e3c77d1a0cb63d0721e2
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-05-29 23:36:18 +0700

    Refactor the code

--- notes-app-back-end/src/handler.js
+++ notes-app-back-end/src/handler.js
@@ -9,7 +9,7 @@ const addNoteHandler = (request, h) => {
   const updatedAt = createdAt;
 
   const newNote = {
-    title, tags, body, id, createdAt, updateAt: updatedAt,
+    title, tags, body, id, createdAt, updatedAt,
   };
 
   notes.push(newNote);
